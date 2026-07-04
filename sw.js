be8ba6df7fb4ad8b6f8a0e560d8fb6741d6048d3
Kauã Redemski de Mello <kauarm@grupo.pietrobon.com.br>
2026-07-04 11:31:43 -0300

    PWA manifest service worker icones

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'pietrobon-v1'
+const ARQUIVOS = [
+  '/index.html',
+  '/CSS/style.css',
+  '/logo.png',
+  '/icon-192.png',
+  '/icon-512.png'
+]
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ARQUIVOS)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) =>
+    Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+  ))
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return
+  if (e.request.url.includes('/api/')) return
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  )
+})
